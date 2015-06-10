b8eb65e14a08f0bee3036775486899b8bc28cc5f
Nathan Giesbrecht <nathan@cnfmarketing.com>
2015-06-10 11:49:54 -0500

    Added basic module install method

--- productcnfoptions.php
+++ productcnfoptions.php
@@ -25,4 +25,12 @@ class ProductCNFOptions extends Module
 		if (!Configuration::get('MYMODULE_NAME'))
 			$this->warning = $this->l('No name provided');
 	}
+
+	public function install()
+	{
+		if(!parent::install())
+			return false;
+
+		return true;
+	}
 }
