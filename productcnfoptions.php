9eb88872ffa790cc8c1bd22c982ed18bed281623
Nathan Giesbrecht <nathan@cnfmarketing.com>
2015-06-10 11:45:45 -0500

    Start of CNF options module

---
+++ productcnfoptions.php
@@ -0,0 +1,28 @@
+<?php
+if (!defined('_PS_VERSION_'))
+  exit;
+
+
+class ProductCNFOptions extends Module
+{
+	public function __construct()
+	{
+		$this->name = 'mymodule';
+		$this->tab = 'cnf_modules';
+		$this->version = '0.0.1';
+		$this->author = 'CNF Marketing';
+		$this->need_instance = 0;
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+		$this->bootstrap = true;
+
+		parent::__construct();
+
+		$this->displayName = $this->l('CNF Product Options');
+		$this->description = $this->l('Adds extra data fields for products.');
+
+		$this->confirmUninstall = $this->l('Are you sure you want to uninstall? Any data saved with this module will be lost!');
+
+		if (!Configuration::get('MYMODULE_NAME'))
+			$this->warning = $this->l('No name provided');
+	}
+}
